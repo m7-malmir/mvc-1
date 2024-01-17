0289d28d10340500fd4731735c8d1e271515985b
Mohammad Malmir <m.malmir666@gmail.com>
2024-01-18 01:33:30 +0330

    work on htaccess and robots text

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
 <?php
+function show($stuff){
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+    function splitURL(){
+        $URL=$_GET['url'] ?? 'home';
+        $URL=explode("/", $URL);
+        return $URL;
+    }
+  function loadController(){
+        $URL=splitURL();
 
-echo 'hello MVC';
+        $filename="../app/controllers/".ucfirst($URL[0]).".php";
+        if(file_exists( $filename)){
+            require_once $filename;
+        }else{
+            $filename="../app/controllers/_404.php";
+           
+                require_once $filename;
+        }
+    }
+
+
+    loadController();
