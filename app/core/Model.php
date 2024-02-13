172ec21cd3cb55e57dcd7d36145fdef4cf1cd51a
Mohamed Malmir <98361393+m7-malmir@users.noreply.github.com>
2024-02-13 10:20:09 +0330

    Update Model.php

---
+++ app/core/Model.php
@@ -0,0 +1,28 @@
+<?php
+
+class Model {
+
+
+use Database;
+
+public function where($data){}
+public function first($data){}
+
+
+public function insert($data){}
+
+
+public function update($id,$data,$id_column=''){}
+
+
+
+public function delete($id,$data,$id_column=''){}
+
+
+
+
+
+
+
+
+}
