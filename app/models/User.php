d73aa017695cf8c330c81cc14becd1097c72a01d
Mohamed Malmir <98361393+m7-malmir@users.noreply.github.com>
2024-02-17 09:20:10 +0330

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -4,4 +4,11 @@ class User{
     use Model;
     protected $table='users';
     protected $allowedColumns=['name','age'];
+
+
+    public function validate($data){
+$this->errors=[];
+      if(empty($this->errors)){return true;}
+return false;
+   }
 }
